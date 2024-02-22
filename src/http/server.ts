e99405c8a9c15ca3a1b9c0b81d45aecf2d175a63
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-22 10:57:46 -0300

    add status code

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,25 @@
 import fastify from "fastify"
 import { z } from "zod"
+import { PrismaClient } from "@prisma/client"
 
 const app = fastify()
 
-app.post("/polls", (request) => {
+const prisma = new PrismaClient()
+
+app.post("/polls", async (request, reply) => {
   const createPollBody = z.object({
     title: z.string(),
   })
 
   const { title } = createPollBody.parse(request.body)
 
-  return "Hello"
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  })
+
+  return reply.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
